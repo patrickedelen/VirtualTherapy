c4131f48e9abafa555a20e9eb74a8c6f7179c2c6
Patrick <patrickedelen@gmail.com>
2016-10-02 11:24:55 -0500

    Finished basic game functionality

---
+++ Virtual Therapy/Assets/Scripts/Flame.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollision(Collider other)
+    {
+        Debug.Log("Trigger fired");
+        GetComponent<ParticleSystem>().Stop();
+    }
+}
