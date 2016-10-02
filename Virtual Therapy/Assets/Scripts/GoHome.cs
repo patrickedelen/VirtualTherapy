510044762aa9e77ea54890ed3ce146ed52ce0ab2
Patrick <patrickedelen@gmail.com>
2016-10-02 07:17:05 -0500

    Started VR integration

---
+++ Virtual Therapy/Assets/Scripts/GoHome.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GoHome : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public void TransitionToMenu()
+    {
+
+        Debug.Log("Changing scene to fence!");
+        SceneManager.LoadScene("Menu");
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
---
+++ Virtual Therapy/Assets/Scripts/Menu.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void TransitionToFence()
+    {
+
+        Debug.Log("Changing scene to fence!");
+        SceneManager.LoadScene("Fence");
+    }
+    public void TransitionToApples()
+    {
+
+        Debug.Log("Changing scene to apples!");
+        SceneManager.LoadScene("trees");
+    }
+    public void TransitionToCards()
+    {
+
+        Debug.Log("Changing scene to cards!");
+        SceneManager.LoadScene("cards");
+    }
+    public void TransitionToCandles()
+    {
+
+        Debug.Log("Changing scene to candles!");
+        SceneManager.LoadScene("Candles");
+    }
+}
